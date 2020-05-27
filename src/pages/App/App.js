5d99b1205e1ead4c30207c038c35d3c0bcd9c26c
pauladixon <paulapdixon@gmail.com>
2020-05-26 22:35:39 -0700

    added footer

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -71,19 +71,22 @@ class App extends React.Component {
               }/>
             </Switch>
           )}
-          </div>
-          <div className='main'>
-            <p className='intro'>
-              A tiny app that lends itself to the moments of clarity and guidance found in the tarot. Utilizing Rachel Howe’s Small Spells tarot deck and writings, we can glimpse the energy of any situation with peace and reception through a simple traditional three-card-pull. We can look at these cards as “past” “present” and ”future” in order to note how we may shift our energies to more gracefully move through the situation in question. 
-            </p>
-            <p className='intro'>
-              To dip into this well, close your eyes, focus on a topic, a question, a feeling, a person.... and with a pure intent for seeking information and clarity in the matter at hand, press go!
-            </p>
-            <Link to='/Reading'>
-              <button className='go'>GO →</button>
-            </Link>
-          </div>
         </div>
+        <div className='main'>
+          <p className='intro'>
+            A tiny app that lends itself to the moments of clarity and guidance found in the tarot. Utilizing Rachel Howe’s Small Spells tarot deck and writings, we can glimpse the energy of any situation with peace and reception through a simple traditional three-card-pull. We can look at these cards as “past” “present” and ”future” in order to note how we may shift our energies to more gracefully move through the situation in question. 
+          </p>
+          <p className='intro'>
+            To dip into this well, close your eyes, focus on a topic, a question, a feeling, a person.... and with a pure intent for seeking information and clarity in the matter at hand, press go!
+          </p>
+          <Link to='/Reading'>
+            <button className='go'>GO →</button>
+          </Link>
+        </div>
+        <footer>
+          Copyright Ⓒ Paula Dixon, 2020
+        </footer>
+      </div>
     ) 
   }
 }
